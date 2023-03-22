1fbc558e502d938267abe39816f159619a6326e1
albertogioia93 <albertogioia@hotmail.it>
2023-03-22 17:57:00 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    components: {
+      AppTitle
+    }
+  }
+
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data (){
+        return {
+            message: 'Ciao'
+        }
+    }
+}
+</script>
+<template>
+    <h1>{{ message }}</h1>
+</template>
+<style scoped></style>
